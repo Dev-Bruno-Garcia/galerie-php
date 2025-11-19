c5abf9e23a86b4181c9362e1924e55ac70a40b3b
Bruno <bgconcept.web@gmail.com>
2025-11-19 19:32:40 +0100

    reset and native

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <!-- Feuilles de style : reset et styles natifs d'abord -->
+    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="assets/css/native.css">
+    <!-- Feuilles de style : variables d'abord, puis styles principaux -->
+    <link rel="stylesheet" href="assets/css/variables.css">
+    <!--finalement les styles principaux -->
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <h1>Lorem ipsum</h1>
+  </body>
 </html>
